d42b317013d80cee90e58fa5bef2f5688badbc64
Valentyna Boriak <valenkiri@gmail.com>
2024-02-22 18:09:22 +0200

    Revert footer back to flexbox on several pages; Add setCustomValidity to the required field (inline); Add a script for primitive validation

---
+++ js/registration-script.js
@@ -0,0 +1,21 @@
+function simpleValidation() {
+  const requiredElements = document
+    .getElementById("signup-form")
+    .querySelectorAll("[required]");
+  const signupBtn = document.getElementById("signup-button");
+  const errorMessage = document.getElementById("error-message");
+  let areRequiredFieldsInvalid = false;
+  for (const elem of requiredElements) {
+    if (!elem.checkValidity()) {
+      areRequiredFieldsInvalid = true;
+      break;
+    }
+  }
+
+  if (areRequiredFieldsInvalid) {
+    errorMessage.innerText = "Please fill the required fields with valid data";
+    console.log(errorMessage);
+  } else {
+    errorMessage.innerText = "";
+  }
+}
